eebcd9e8d78a1e0e216a6fd855e2163ac4374568
Isaac Shafer <izakshafer@gmail.com>
2022-03-17 15:01:09 -0600

    added rollbar hopefully

--- server.js
+++ server.js
@@ -6,5 +6,16 @@ app.use(express.json())
 app.use(cors())
 app.use(express.static(path.join(__dirname, './public')))
 
+// include and initialize the rollbar library with your access token
+var Rollbar = require('rollbar')
+var rollbar = new Rollbar({
+  accessToken: 'dbf039b4fa694e8ebc66a4249e32955d',
+  captureUncaught: true,
+  captureUnhandledRejections: true,
+})
+
+// record a generic message and send it to Rollbar
+rollbar.log('Hello world!')
+
 const port = process.env.PORT || 5500
 app.listen(port, ()=> console.log(`server running on ${port}`))
